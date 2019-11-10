7468bba03e9e1fdecf121751c1832d8c3b3a8179
Luis  Machado <luismachado@MacBook-Pro-de-Luis.local>
2019-11-10 12:00:25 -0300

    Inital Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
